b73f542e94b13ac8292cd8985e6dc8f46e1740fb
bhavini <bhavini266@gmail.com>
2019-11-25 22:46:07 -0600

    modified file

--- MyApp.py
+++ MyApp.py
@@ -2,16 +2,16 @@ import os
 import psycopg2
 from flask import Flask
 
-my_awesome_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@my_awesome_app.route('/')
+@app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    my_awesome_app.run()
+    app.run()
     
 print("Opening Database connection")
 
