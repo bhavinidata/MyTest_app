920ff8fdf0746318b093975f23ca1399c8962079
bhavini <bhavini266@gmail.com>
2019-11-25 22:41:10 -0600

    modified file

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import os
 import psycopg2
+from flask import Flask
 
+my_awesome_app = Flask(__name__)
+
+
+@my_awesome_app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    my_awesome_app.run()
+    
 print("Opening Database connection")
 
 DATABASE_URL = os.environ['DATABASE_URL']
